ae572b521c29a60226fd5b0511b9cf9f3b15fe2f
Reino Muhl <reino.muhl@gmail.com>
2023-07-18 20:01:06 -0400

    Add graphql env var

--- apps/future-search/apollo-client.ts
+++ apps/future-search/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:6226/graphql',
+  uri: process.env.NEXT_PUBLIC_API_GRAPHQL_URL,
   cache: new InMemoryCache(),
 });
 
